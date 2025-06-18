eabdffedd5509ded1aa5aa90466d7c7495a31cf9
André Gardi <gardikoury@gmail.com>
2025-06-18 09:12:01 -0300

    Save snippet on database

---
+++ server/src/models/snippet.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface SnippetDocument extends Document {
+  text: string;
+  summary: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const SnippetSchema = new Schema<SnippetDocument>(
+  {
+    text: { type: String, required: true },
+    summary: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const Snippet = mongoose.model<SnippetDocument>('Snippet', SnippetSchema);
